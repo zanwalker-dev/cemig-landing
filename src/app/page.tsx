f35ed4ac5420141db4abeaab0a49340c13618a4a
zanwalker-dev <andre.rocha2302@gmail.com>
2025-10-27 10:06:53 -0300

    cleaning:deleting import unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-// src/app/page.tsx
-import { HeroSection } from "@/components/sections/HeroSection";
 import { TrustSignalsSection } from "@/components/sections/TrustSignalsSection";
 import { AchievementsSection } from "@/components/sections/AchievementsSection";
 import { LeadFormSection } from "@/components/sections/LeadFormSection";
